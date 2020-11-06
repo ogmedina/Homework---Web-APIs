b888890ac0ebd0328dd0e614106fc5666af37754
Oscar Medina <ogmedina04@gmail.com>
2020-11-06 14:24:52 -0600

    finished code, working on way to sort high scores, need to finish screenshots

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,23 @@
+//Variables
 var timerEl = document.querySelector("#timer");
 var startBtnEl = document.querySelector("#startbtn");
-var mainContentEl = document.querySelector(".mainContent");
+var mainContentEl = document.querySelector("#mainContent");
 var answerBarEl = document.querySelector(".answerBar");
-
-
+var finalScoreEl = document.querySelector("#finalScore");
+var clearScoresBtn = document.querySelector("#clear");
+var questionCount = 0;
+var gameOverHeader;
+var gameOverBody;
+var gameOverForm;
+var gameOverFormLabel;
+var gameOverFormInput;
+var formValue;
+var questionEl;
+var answerEl;
 var countDown = 90;
+var pointsArr = [];
+
+//Questions Array
 var questionArr = [
     {
         question: "Commonly used data types do NOT include: ",
@@ -49,28 +62,189 @@ var questionArr = [
             "2. terminal/bash",
             "3. for loops",
             "4. console.log()"],
-        correctAnser: 3,
+        correctAnswer: 3,
     },
 ];
 
-var currentIndex = 0;
-//Timer count down, 
+//Start Button Event Listener -- Click button to start quiz function
+if (startBtnEl !== null){
+    startBtnEl.addEventListener("click", startQuiz);
+}
+//Timer count down, and start of interval countdown. When the countdown reaches 0 questionCount is set to 5 and the function
+//renderQuestion will send you to the gameOver() function 
 function startQuiz(){
     interval = setInterval(function (){
         countDown--;
-        timerEl.innterHTML = "Time: " + countDown;
+        timerEl.innerHTML = "Timer: " + countDown;
         if (countDown <= 0){
-            clearInterval(interval);
-            questionCount =5;
+            clearInterval(interval);   
+            questionCount = 5;
             renderQuestion();
         }
     }, 1000);
-
     renderQuestion();
 }
 
+//Main component that matches buttons and compares answer with correct answer listed in index of the question array and sends to the functions rightAnswer or wrongAnswer
+mainContentEl.addEventListener("click", function (event){
+    var element = event.target;    
+    if (element.matches("button")){
+        if (element.dataset.index == questionArr[questionCount]["correctAnswer"]){
+            rightAnswer();
+        }
+        else if (!element.hasAttribute("data-index")) {
+        }
+        else {
+            wrongAnswer();
+        }
+    }
+});
+
+// Question Renders with appropriate buttons and timer and re-writes the screen with the h4 and div buttons
 function renderQuestion() {
     mainContentEl.innerHTML = "";
-    
+    if (questionCount > 4){
+        clearInterval(interval);
+        timerEl.innerHTML = "Time: " + countDown;
+        gameOver();
+    }
+    else {
+    questionEl = document.createElement("h4");    
+    questionEl.innerText = questionArr[questionCount]["question"];
+    mainContentEl.append(questionEl);
+    divEl = document.createElement("div");
+    divEl.setAttribute("class", "divEl btn-group-vertical");
+    for (var i = 0; i < questionArr[questionCount]["answers"].length; i++){
+        answersEl = document.createElement("button");        
+        answersEl.innerText = questionArr[questionCount]["answers"][i];
+        answersEl.setAttribute("class", "btn-group-vertical quizBtn");
+        answersEl.setAttribute("style", "float: left")              
+        answersEl.setAttribute("data-index", i);
+        divEl.appendChild(answersEl);
+        }
+    mainContentEl.append(divEl);
+    }
 }
+
+// Function for wrong answers and the change of HTML and the subtraction of time
+function wrongAnswer(){
+    var wrongResponse;
+    answerBarEl.innerHTML = "";
+    wrongResponse = document.createElement("div");
+    wrongResponse.innerText = "Wrong!";
+    wrongResponse.setAttribute("class","answerBar");
+    wrongResponse.setAttribute("style", "border-top: lightgray solid 2px");
+    wrongResponse.style.backgroundColor = "red";
+    wrongResponse.style.color = "white";
+    wrongResponse.style.fontWeight = "bold";
+    wrongResponse.style.textAlign = "center";
+    answerBarEl.append(wrongResponse);
+    setTimeout(function (){
+        wrongResponse.innerHTML = "";
+        wrongResponse.setAttribute("style", "border-top: none");
+    }, 1000);
+    countDown -= 10;
+    questionCount++;
+    
+    renderQuestion();    
+}
+// Function for right answers and the change of HTML, NO subtraction from time
+function rightAnswer(){
+    var correctResponse;
+    answerBarEl.innerHTML = "";
+    correctResponse = document.createElement("div");
+    correctResponse.innerText = "Correct!";
+    correctResponse.setAttribute("class", "answerBar");
+    correctResponse.setAttribute("style", "border-top: lightgray solid 2px");
+    correctResponse.style.backgroundColor = "green";
+    correctResponse.style.color = "black";
+    correctResponse.style.fontWeight = "bold";
+    correctResponse.style.textAlign = "center";
+    answerBarEl.append(correctResponse);
+    setTimeout(function(){
+        correctResponse.innerHTML = "";
+        correctResponse.setAttribute("style", "border-top: none");
+    }, 1000);
+    questionCount++;    
+    renderQuestion();
+}
+
+//Game Over function, crates page with classes and score is the countdown
+function gameOver(){
+    gameOverHeader = document.createElement("h4");
+    gameOverHeader.setAttribute("class", "mainContent");
+    gameOverHeader.innerText = "Quiz Completed!!!!";
+    gameOverBody = document.createElement("div");
+    gameOverBody.innerText = "Your final score is: " + countDown; 
+    gameOverBody.setAttribute("style", "font-weight: normal");
+    gameOverHeader.append(gameOverBody);
+    gameOverForm = document.createElement("form");
+    gameOverForm.setAttribute("style", "padding: 20px");
+    gameOverFormLabel = document.createElement("label");
+    gameOverFormLabel.innerText = "Enter Initials: ";
+    gameOverForm.append(gameOverFormLabel);
+    gameOverFormInput = document.createElement("input");
+    gameOverFormInput.setAttribute("class", "form-control");
+    gameOverFormInput.setAttribute("type", "text");
+    gameOverFormInput.setAttribute("pattern", "[A-Za-z]{3}");
+    gameOverFormInput.setAttribute("id", "formInput"); //capitalize input?
+    gameOverFormInput.setAttribute("placeholder", "Initials Here (Letters ONLY) press ENTER when done");
+    gameOverForm.append(gameOverFormInput);
+    gameOverBody.append(gameOverForm);
+    mainContentEl.append(gameOverHeader);
+}
+
+//calls high score page function
+highScoresPage();
+
+//Function for high scores page, puts the high scores that have been saved
+//work out sort function! 
+function highScoresPage(){   
+    if (finalScoreEl !== null){
+        var combinedScore = JSON.parse(localStorage.getItem("scoreArr"));
+        finalScoreEl.innerHTML = "";
+        for (i = 0; i < combinedScore.length; i += 2){
+            var scoreAppend = document.createElement("div");
+            scoreAppend.setAttribute("class", "highScores");
+           scoreAppend.innerText = i / 2 + 1 + ". " + combinedScore[i] + " " + combinedScore[i + 1];
+          // myFunction();
+          // function myFunction(){
+           //    combinedScore[i + 1].sort(function(a,b){return a-b});               
+           //};
+           
+           finalScoreEl.append(scoreAppend);
+          // scoreArr.sort((a, b) => b-a);
+        }
+    }
+}
+//Function that waits for submit by enter button to store new score and then switches to the quizhighscores html
+mainContentEl.addEventListener("submit", function(event){    
+    event.preventDefault();    
+    var element = event.target;
+    formValue = gameOverFormInput.value;
+    if (element.matches("form")) {
+        if (JSON.parse(localStorage.getItem("scoreArr") == null)){
+            pointsArr = [];
+        } else{
+            pointsArr = JSON.parse(localStorage.getItem("scoreArr"));
+        }
+        
+        pointsArr.push(formValue);
+        pointsArr.push(countDown);
+        
+        localStorage.setItem("scoreArr", JSON.stringify(pointsArr));
+        window.location.href = "./quizhighscores.html";
+        
+    }
+
+});
+
+//Clears score Array in local storage
+if (clearScoresBtn !== null) {
+    clearScoresBtn.addEventListener("click", function (){
+        localStorage.setItem("scoreArr", JSON.stringify([]));
+        highScoresPage();
+    });
+}
+
 
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Homework---Web-APIs
-Bootcamp Homework 3 - Web APIs - Quiz
+# Homework Web-APIs: Code Quiz
+
+## Full Stack Boot Camp Homework 4 - Code Quiz
+
+### Description
+
+This is the fourth assignment for the UTSA Full-Stack Bootcamp. This is a Code Quiz using JavaScript. The following items were the highlights of this assignment:
+
+* Timed Coding Quiz
+
+* Multiple-Choice Questions
+
+* Run in broswer
+
+* Dynamically updated HTML and CSS powered by JavaScript Code
+
+* Clean, polished, and responsive user interface
+
+### Screenshots
+
+Here is a screenshot of the Code Quiz. 
+
+(coming soon!)
+
+
+### Deployed Link
+
+Here is a link to the GitHub deployed site: [Code Quiz](https://ogmedina.github.io/Homework---Web-APIs/)
